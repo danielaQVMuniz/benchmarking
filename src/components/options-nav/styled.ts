d9325f9278a3c7f50c12e989688966d4a4139844
Daniela Muniz <danielaqvmuniz@gmail.com>
2025-06-12 13:11:10 -0400

    fixup

--- src/components/options-nav/styled.ts
+++ src/components/options-nav/styled.ts
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 export const OptionsNavContainer = styled.aside`
   height: calc(100vh - 65px);
-  overflow: scroll;
 `
 
 export const CustomTabContainer = styled.div`
   background: linear-gradient(90deg, #edede9 0%, hsla(192, 17%, 94%, 1) 100%);
+  height: 100%;
   min-height: calc(100% - 49px);
+  overflow: auto;
+}
 `
